010eadc7ff6d32eb4569c20a0fcdee0de16accab
Grey-Eden <70287071+Grey-Eden@users.noreply.github.com>
2020-09-09 15:09:59 -0400

    Just added all my unity project files

---
+++ Unity/Assets/Script/GridMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+	public Transform playerTransform;
+	public Vector3 fwd,bwd,lft,rgt,up,dwn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //The 6-directional movement	
+    	if(Input.GetKeyDown(KeyCode.W)){
+    		playerTransform.position += fwd;
+    	}
+    	if(Input.GetKeyDown(KeyCode.S)){
+    		playerTransform.position += bwd;
+    	}
+    	if(Input.GetKeyDown(KeyCode.A)){
+    		playerTransform.position += lft;
+    	}
+    	if(Input.GetKeyDown(KeyCode.D)){
+    		playerTransform.position += rgt;	
+    	}
+    	if(Input.GetKeyDown(KeyCode.Q)){
+    		playerTransform.position += up;	
+    	}
+    	if(Input.GetKeyDown(KeyCode.E)){
+    		playerTransform.position += dwn;	
+    	}
+    //Attempted player reset if passed the playable grid
+    	if(playerTransform.position.lft<0){
+    		playerTransform.position += rgt;
+
+    	}	
+    }
+}
